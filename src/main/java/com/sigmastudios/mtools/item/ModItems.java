b07eea76e5723ee2d334a49ed85da7e9e2d07ad6
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-10-31 21:39:52 +0100

    Added Ruby Axe

--- src/main/java/com/sigmastudios/mtools/item/ModItems.java
+++ src/main/java/com/sigmastudios/mtools/item/ModItems.java
@@ -1,5 +1,6 @@
 package com.sigmastudios.mtools.item;
 
+import com.sigmastudios.mtools.item.Tools.RubyTools.RubyAxe;
 import com.sigmastudios.mtools.item.Tools.RubyTools.RubySword;
 import com.sigmastudios.mtools.item.ore.RubyIngot;
 import com.sigmastudios.mtools.item.Tools.RubyTools.RubyPickaxe;
@@ -11,19 +12,23 @@ public class ModItems
 {
     public static Item.ToolMaterial RUBYPICKAXE = EnumHelper.addToolMaterial("JEFFPICKAXE", 3, 1350, 8.0F, 2.5F, 5);
     public static Item.ToolMaterial RUBYSWORD = EnumHelper.addToolMaterial("RUBYSWORD", 3, 500, 8.0F, 7.5F, 4);
+    public static Item.ToolMaterial RUBYAXE = EnumHelper.addToolMaterial("RUBYAXE", 3, 250, 8.0F, 3.75F, 2);
 
     public static RubyPickaxe rubyPickaxe = new RubyPickaxe(RUBYPICKAXE, "ruby_pickaxe");
     public static RubySword rubySword = new RubySword(RUBYSWORD, "ruby_sword");
+    public static RubyAxe rubyAxe = new RubyAxe(RUBYAXE, 3.75F, 1, "ruby_axe");
     public static RubyIngot rubyIngot = new RubyIngot("ruby_ingot");
 
     public static void register(IForgeRegistry<Item> registry)
     {
-        registry.registerAll(rubyPickaxe, rubySword, rubyIngot);
+        registry.registerAll(rubyIngot, rubySword, rubyPickaxe, rubyAxe);
     }
     public static void registerModels ()
     {
-        rubyPickaxe.registerItemModel("ruby_pickaxe");
         rubySword.registerItemModel("ruby_sword");
+        rubyPickaxe.registerItemModel("ruby_pickaxe");
+        rubyAxe.registerItemModel("ruby_axe");
+
         rubyIngot.registerItemModel("ruby_ingot");
     }
 }
---
+++ src/main/java/com/sigmastudios/mtools/item/Tools/RubyTools/RubyAxe.java
@@ -0,0 +1,26 @@
+package com.sigmastudios.mtools.item.Tools.RubyTools;
+
+import com.sigmastudios.mtools.MoreTools;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+
+public class RubyAxe extends ItemAxe
+{
+    public RubyAxe(ToolMaterial material, float damage, float speed, String name)
+    {
+        super(material, damage, speed);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(MoreTools.tab);
+    }
+    @Override
+    public EnumRarity getRarity(ItemStack stack)
+    {
+        return EnumRarity.RARE;
+    }
+    public void registerItemModel(String name)
+    {
+        MoreTools.proxy.registerItemRenderer(this, 0, name);
+    }
+}
