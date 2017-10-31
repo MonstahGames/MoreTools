478aa51ab7216a4479e00611656faa03a6e51c54
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-10-31 15:29:52 +0100

    Tab and Proxy

--- src/main/java/com/sigmastudios/mtools/MoreTools.java
+++ src/main/java/com/sigmastudios/mtools/MoreTools.java
@@ -1,18 +1,48 @@
 package com.sigmastudios.mtools;
 
+import com.sigmastudios.mtools.proxy.CommonProxy;
+import com.sigmastudios.mtools.tab.CreativeTab;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MoreTools.MODID, version = MoreTools.VERSION)
+@Mod(modid = MoreTools.MODID, version = MoreTools.VERSION, name = MoreTools.NAME)
 public class MoreTools
 {
     public static final String MODID = "mtools";
     public static final String VERSION = "1.0";
-    
+    public static final String NAME = "More Tools";
+
+    public static final String CLProxy = "com.sigmastudios.mtools.proxy.ClientProxy";
+    public static final String CProxy = "com.sigmastudios.mtools.proxy.CommonProxy";
+
+    @SidedProxy(clientSide = CLProxy, serverSide = CProxy)
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static MoreTools instance;
+
+    public static CreativeTab tab;
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void preInit(FMLPreInitializationEvent event)
     {
+        tab = new CreativeTab(CreativeTabs.getNextID(), "mtools_tab");
+        proxy.preInit(event);
+    }
+    @EventHandler
+    public void init (FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+    @EventHandler
+    public  void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
     }
 }
---
+++ src/main/java/com/sigmastudios/mtools/proxy/ClientProxy.java
@@ -0,0 +1,32 @@
+package com.sigmastudios.mtools.proxy;
+
+import com.sigmastudios.mtools.MoreTools;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy
+{
+    @Override
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Override
+    public void init(FMLInitializationEvent event)
+    {
+    }
+    @Override
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+
+    @Override
+    public void registerItemRenderer(Item item, int meta, String id)
+    {
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(MoreTools.MODID + ":" + id, "inventory"));
+    }
+}
