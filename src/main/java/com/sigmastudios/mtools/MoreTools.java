0f87279408d17790e55f24f223434bca4e92c2ec
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-10-31 15:15:55 +0100

    Renamed to More Tools

--- src/main/java/com/sigmastudios/mtools/MoreTools.java
+++ src/main/java/com/sigmastudios/mtools/MoreTools.java
@@ -1,20 +1,18 @@
-package com.example.examplemod;
+package com.sigmastudios.mtools;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MoreTools.MODID, version = MoreTools.VERSION)
+public class MoreTools
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mtools";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
     }
 }
