2a8b247b7542c1b9616f30b50ad3fc80fa006a73
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-01 10:46:27 +0100

    Fixed some line endings and removed unnessecary things

--- src/main/java/com/sigmastudios/mtools/item/ore/WorldOreGen.java
+++ src/main/java/com/sigmastudios/mtools/item/ore/WorldOreGen.java
@@ -2,8 +2,6 @@ package com.sigmastudios.mtools.item.ore;
 
 import com.google.common.base.Predicate;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.block.state.pattern.BlockMatcher;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -20,23 +18,23 @@ public class WorldOreGen implements IWorldGenerator
     {
         int dimension = world.provider.getDimension();
         {
-            if (dimension == 0) {
+            if (dimension == 0)
+            {
                 //genSurfaceMoreTools(world, random, chunkX * 16, chunkZ * 16);
-            } else {
             }
         }
     }
-    //private void genSurfaceMoreTools(World world, Random random, int chunkX, int chunkZ) {
-    //    addOreSpawn("en hier", world, random, chunkX, chunkZ, 16, 16, 10, 2, 0, 10,
-    //            BlockMatcher.forBlock(Blocks.STONE));
-    //    addOreSpawn("ZET BLOCK HIER".getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 10, 10, 0, 5,
+    //private void genSurfaceMoreTools(World world, Random random, int chunkX, int chunkZ)
+    // {
+    //    addOreSpawn("[BLOCK HERE]".getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 10, 10, 0, 5,
     //            BlockMatcher.forBlock(Blocks.STONE));
     //}
 
-    private void addOreSpawn(IBlockState block, World world, Random random, int blockXPos, int blockZPos, int maxX,
-                             int maxZ, int maxVeinSize, int chance, int minY, int maxY, Predicate<IBlockState> blockToSpawnIn) {
+    private void addOreSpawn(IBlockState block, World world, Random random, int blockXPos, int blockZPos, int maxX, int maxZ, int maxVeinSize, int chance, int minY, int maxY, Predicate<IBlockState> blockToSpawnIn)
+    {
         int diffMinMaxY = maxY - minY;
-        for (int x = 0; x < chance; x++) {
+        for (int x = 0; x < chance; x++)
+        {
             int posX = blockXPos + random.nextInt(maxX);
             int posY = minY + random.nextInt(diffMinMaxY);
             int posZ = blockZPos + random.nextInt(maxZ);
